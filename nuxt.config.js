8ef1e5571330924f39e7710548954f55c4279bcf
Arnold <sebastianbnec@gmail.com>
2022-02-08 12:29:30 +0700

    Remove conditional check

--- nuxt.config.js
+++ nuxt.config.js
@@ -137,7 +137,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     proxy: development,
-    baseURL: development ? '' : process.env.API_URL,
+    baseURL: process.env.API_URL,
   },
 
   // Proxy configuration: https://axios.nuxtjs.org
