9d5681a9d5ed65144a72e83e5256e3a7cd4769e0
Rafi <rafi@Rafis-MacBook-Pro.local>
2022-06-03 15:37:00 +0700

    [ENHANCE]: add user_id and email on gtag

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,8 +1,12 @@
+const dataUser = JSON.parse(localStorage.getItem('auth.user') as any);
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'G-23254NLRBW');
+gtag('config', 'G-23254NLRBW', {
+  'user_id': dataUser?.clientId ?? '',
+  'email': dataUser?.email ?? ''
+});
 
 // export default ({ $config: { googleAnalytics } }) => {
 //   gtag('config', googleAnalytics.id);
